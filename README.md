94fd270436abc451606d9c5ea119d2b92846e12b
JoseAntonio76 <josem21s1133@gmail.com>
2026-07-10 19:41:59 -0300

    mudei o py

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 
 # ola, mundo.
-mudei o project
--- expython/python.py
+++ expython/python.py
@@ -5,5 +5,5 @@ class Carro():
     def verificar():
         print(f'o ano do seu carro e {self.ano} e a marca e {self.marca_roda}')
 
-p1=Carro('michelin',2020)
+p1=Carro('michelin',1999)
 p1.verificar()
