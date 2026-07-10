c2cf06b2df9ba94360c769e799098904489a1c01
JoseAntonio76 <josem21s1133@gmail.com>
2026-07-10 19:35:36 -0300

    Create python.py

---
+++ expython/python.py
@@ -0,0 +1,9 @@
+class Carro():
+    def __init__(self,marca_roda,ano):
+        self.marca_roda=marca_roda
+        self.ano=ano
+    def verificar():
+        print(f'o ano do seu carro e {self.ano} e a marca e {self.marca_roda}')
+
+p1=Carro('michelin',2020)
+p1.verificar()
